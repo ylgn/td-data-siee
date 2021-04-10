745ef2b198a52b47487fb074dc44bb85a9865df0
ylgn <yannis.belkebir@etu.parisdescartes.fr>
2021-04-10 20:30:07 +0200

    V3 & last version, add of statistics

--- main_ylgn.py
+++ main_ylgn.py
@@ -1,6 +1,8 @@
 import urllib.request
 import pandas as pd
 import lxml.html
+from re import sub
+from decimal import Decimal
 
 #######################################
 # Get all car from berline comptacte on aramisauto.com
@@ -13,40 +15,72 @@ def mreplace(sentence):
 
 
 
+
+
 URL = 'https://www.aramisauto.com/achat/berline-compacte/'
+NB_PAGES_MAX = 25
 p= 1
+print(f'This program scraps every berline cars from page {p} to {NB_PAGES_MAX} on aramisauto website.')
 headers = {}
 current_URL =""
 titles = []
 prices = []
+motorisation = []
+transmission = []
+
+
+
 
 uagent = ('Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like '
           'Gecko) Chrome/24.0.1312.27 Safari/537.17')
 headers['User-Agent'] = uagent
 
-while (p <=25):
-	current_URL = URL+"?page="+str(p)
-	print(current_URL)
-	request = urllib.request.Request(current_URL, headers=headers)
-	resp = urllib.request.urlopen(request)
-	page = resp
-	html = lxml.html.fromstring(page.read())
+print('Processing please wait....')
+
+#GET ALL INFORMATION FROM THE CARS
+while (p <= NB_PAGES_MAX):
+    print(f'* Scraping page {p} on {NB_PAGES_MAX}')
+    current_URL = URL+"?page="+str(p)
+    request = urllib.request.Request(current_URL, headers=headers)
+    resp = urllib.request.urlopen(request)
+    page = resp
+    html = lxml.html.fromstring(page.read())
 	## Version avec seulement xpath (expert)
-	print(html.xpath('//span[@class="vehicle-model"]'))
-	titles.extend((html.xpath('//span[@class="vehicle-model"]')))
-	prices.extend((html.xpath('//span[@class="vehicle-loa-offer"]')))
-	p = p + 1
-	pass
+    titles.extend((html.xpath('//span[@class="vehicle-model"]')))
+    prices.extend((html.xpath('//span[@class="vehicle-loa-offer"]')))
+    motorisation.extend((html.xpath('//div[@class="vehicle-motorisation"]')))
+    transmission.extend((html.xpath('//div[@class="vehicle-transmission"]')))
+    p = p + 1
+    pass
 
-
-
-results = {k: {'title': mreplace(titles[k].text_content()),
-               'price': mreplace(prices[k].text_content())}
+results = {k: {'car': mreplace(titles[k].text_content()),
+               'price': float(Decimal(sub(r'[^\d.]', '', prices[k].text_content()))),       
+               'motorisation': mreplace(motorisation[k].text_content()),
+               'transmission': mreplace(transmission[k].text_content())}
+               
            for k in range(len(titles))}
-
 
 # Save in xlsx
 df = pd.DataFrame.from_dict(results, orient='index')
-xlsx_file = "out.xlsx"
+df.sort_values(by=['price','car','motorisation'], inplace=True)
+
+nbCars = len(df.index)
+cheapestCar = {'car' : df["car"].values[0],'price' : df["price"].values[0],'motorisation' : df["motorisation"].values[0],'transmission' : df["transmission"].values[0]}
+expensiveCar = {'car' : df["car"].values[nbCars-1],'price' : df["price"].values[nbCars-1],'motorisation' : df["motorisation"].values[nbCars-1],'transmission' : df["transmission"].values[nbCars-1]}
+averagePrice = round(df["price"].mean(),1)
+
+df.loc[len(df)]=['AVERAGE PRICE',averagePrice,'NUMBER OF CARS',nbCars]
+df.loc[len(df)]=['CHEAPEST CAR IS :',cheapestCar["car"],'MOST EXPENSIVE CAR CAR IS :',expensiveCar["car"]]  
+xlsx_file = "out_cars.xlsx"
 df.to_excel(xlsx_file)
-print(f'{df.shape} df saved as {xlsx_file}')
+
+
+
+
+print(f'\n{nbCars} cars has been found and sorted by ascending price for UX purposes.\n')
+print(f'The cheapest car is the {cheapestCar["car"]}, it costs {cheapestCar["price"]} €. It is equipped by {cheapestCar["motorisation"]} engine and has a {cheapestCar["transmission"]} transmission.\n') 
+print(f'The most expensive car is the {expensiveCar["car"]}, it costs {expensiveCar["price"]} €. It is equipped by {expensiveCar["motorisation"]} engine and has a {expensiveCar["transmission"]} transmission.\n')     
+     
+print(f'On average on this website, a second-hand compact berline car costs {averagePrice}€\n')
+
+print(f'All this information as been fomalised into a matrix {df.shape} and saved as {xlsx_file}')
